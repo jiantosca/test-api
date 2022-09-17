7e2f6ffcc3e770912cdc0a1363a891921a51f07c
jiantosca <jiantosca@gmail.com>
2022-09-16 22:10:29 -0400

    build

--- src/main/java/com/jonnyi/api/MainApplication.java
+++ src/main/java/com/jonnyi/api/MainApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MainApplication.class, args);
+
     }
 }
